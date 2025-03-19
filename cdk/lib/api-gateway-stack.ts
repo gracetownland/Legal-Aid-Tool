e9b76d1476fddb8bf9ca3f215a91678d1f55adf4
Zayan Sheikh <dbzayan@gmail.com>
2025-03-19 02:06:39 -0700

    fix: update SM_DB_CREDENTIALS to use secretPathAdminName and add vpc to rdsProxyAdmin in DatabaseStack

--- cdk/lib/api-gateway-stack.ts
+++ cdk/lib/api-gateway-stack.ts
@@ -891,7 +891,7 @@ export class ApiGatewayStack extends cdk.Stack {
         vpc: vpcStack.vpc, // Pass the VPC
         functionName: `${id}-TextGenLambdaDockerFunction`,
         environment: {
-          SM_DB_CREDENTIALS: db.secretPathUser.secretName,
+          SM_DB_CREDENTIALS: db.secretPathAdminName,
           RDS_PROXY_ENDPOINT: db.rdsProxyEndpointAdmin,
           REGION: this.region,
           BEDROCK_LLM_PARAM: bedrockLLMParameter.parameterName,
