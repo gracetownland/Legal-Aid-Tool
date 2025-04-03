0a0b1749cfb9f91b0ba4a68157daf54c3104efcc
Zayan Sheikh <dbzayan@gmail.com>
2025-04-03 13:55:06 -0700

    fix: replace StudentHeader with AdminHeader in AdminChangeSystemPrompt component

--- frontend/src/pages/admin/AdminChangeSystemPrompt.jsx
+++ frontend/src/pages/admin/AdminChangeSystemPrompt.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { fetchAuthSession } from "aws-amplify/auth";
 import { Button, TextField, Card, CardContent, Typography, IconButton } from "@mui/material";
-import StudentHeader from "../../components/StudentHeader";
+import AdminHeader from "../../components/AdminHeader";
 import HistoryIcon from '@mui/icons-material/History';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
@@ -100,7 +100,7 @@ export default function EditSystemPrompts() {
 
   return (
     <div className="max-w-4xl mx-auto p-4">
-      <StudentHeader />
+      <AdminHeader />
 
       {/* Current Prompt */}
       <Card sx={{ 
