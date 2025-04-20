28886fc90c7937676cb5925d33e5f8fe36781e7b
zayan-sheikh <dbzayan@gmail.com>
2025-04-20 01:11:58 -0700

    Refactor NotFound page with updated UI and navigation

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,24 +1,44 @@
 import React, { useState, useEffect } from "react";
-import StudentHeader from "../components/StudentHeader";
-import InstructorHeader from "../components/InstructorHeader";
-import AdminHeader from "../components/AdminHeader";
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import Home from "@mui/icons-material/Home";
 
 const NotFound = () => {
-  const [userRole, setUserRole] = useState(null);
-
-  useEffect(() => {
-    const fetchUserRole = async () => {
-      
-    };
-    fetchUserRole();
-  }, []);
-
+  const navigate = useNavigate();
   return (
-    <div style={{ padding: "2em", textAlign: "center", fontFamily: "Outfit", display: 'flex', flexDirection: 'column', gap: '1em', color: 'var(--header-text)'}}>
-        {/* ((userRole === "student") ? <StudentHeader /> : <InstructorHeader />) */}
-        <StudentHeader />
+    <div
+      style={{
+        padding: "2em",
+        textAlign: "center",
+        fontFamily: "Outfit",
+        display: "flex",
+        flexDirection: "column",
+        gap: "1.5em",
+        color: "var(--header-text)",
+        alignItems: "center",
+      }}
+    >
       <h1>Page Not Found</h1>
       <p>Sorry, the page you're looking for doesn't exist.</p>
+      <Button
+        onClick={() => navigate("/home")}
+        variant="filled"
+        sx={{
+          backgroundColor: "var(--primary)",
+          width: "12em",
+          borderRadius: "2em",
+          color: "white",
+          fontFamily: "Outfit",
+          boxShadow: "none",
+          "&:hover": {
+            backgroundColor: "var(--secondary)",
+          },
+        }}
+        startIcon={<Home />}
+        
+      >
+        Back to Home
+      </Button>
     </div>
   );
 };
