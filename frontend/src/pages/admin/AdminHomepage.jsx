2f3f5482b9b8beb59a047bc87d03fac0ab30fb78
Prajna <prajnapn36@gmail.com>
2025-04-24 15:05:40 -0700

    fix: imports for admin homepage

--- frontend/src/pages/admin/AdminHomepage.jsx
+++ frontend/src/pages/admin/AdminHomepage.jsx
@@ -2,11 +2,8 @@
 import AdminSidebar from "./AdminSidebar";
 import AdminHeader from "../../components/AdminHeader";
 import AdminInstructors from "./AdminInstructors";
-import AdminSimulationGroups from "./AdminSimulationGroups";
-import AdminCreateSimulationGroup from "./AdminCreateSimulationGroup";
 import PageContainer from "../Container";
 import InstructorDetails from "./InstructorDetails";
-import GroupDetails from "./GroupDetails";
 // MUI
 import { AppBar } from "@mui/material";
 import { useState } from "react";
@@ -15,7 +12,6 @@ export const AdminHomepage = () => {
   const [selectedComponent, setSelectedComponent] =
     useState("AdminInstructors");
   const [selectedInstructor, setSelectedInstructor] = useState(null);
-  const [selectedGroup, setSelectedGroup] = useState(null);
 
   // sidebar routing
   const renderComponent = () => {
@@ -27,24 +23,11 @@ export const AdminHomepage = () => {
         />
       );
     }
-    if (selectedGroup) {
-      return (
-        <GroupDetails
-          group={selectedGroup.row}
-          onBack={() => setSelectedGroup(null)}
-        />
-      );
-    }
+    
     switch (selectedComponent) {
       case "AdminInstructors":
         return (
           <AdminInstructors setSelectedInstructor={setSelectedInstructor} />
-        );
-      case "AdminSimulationGroups":
-        return <AdminSimulationGroups setSelectedGroup={setSelectedGroup} />;
-      case "AdminCreateSimulationGroup":
-        return (
-          <AdminCreateSimulationGroup setSelectedComponent={setSelectedComponent} />
         );
       default:
         return (
